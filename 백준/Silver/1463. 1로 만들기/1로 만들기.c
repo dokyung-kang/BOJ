52a7333e369cc2f9a354f58fb8a57028f5614898
dokyung-kang <kangdk02@naver.com>
2022-08-15 00:41:14 +0900

    [Silver III] Title: 1로 만들기, Time: 32 ms, Memory: 32244 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+long long cal(long long n, long long* M)
+{
+
+	long long min = n;
+	if (n == 1)
+		return 0;
+	if (n % 3 == 0) {
+		if (M[n / 3] == -1)
+			M[n / 3] = cal(n / 3, M);
+		if (M[n / 3] < min)
+			min = M[n / 3];
+	}
+	if (n % 2 == 0) {
+		if (M[n / 2] == -1)
+			M[n / 2] = cal(n / 2, M);
+		if (M[n / 2] < min)
+			min = M[n / 2];
+	}
+	if (M[n - 1] == -1)
+		M[n - 1] = cal(n - 1, M);
+	if (M[n - 1] < min)
+		min = M[n - 1];
+	return min + 1;
+}
+
+int main(void)
+{
+	long long n;
+	long long* M;
+	int i;
+
+	scanf("%lld", &n);
+	M = (long long*)malloc(sizeof(long long) * n);
+	for (i = 0; i < n; i++)
+		M[i] = -1;
+	printf("%lld", cal(n, M));
+}
