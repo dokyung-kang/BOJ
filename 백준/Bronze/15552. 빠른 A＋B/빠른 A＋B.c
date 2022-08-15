42aae7d57cc7cbb9042998fa108c5bd2762cd744
dokyung-kang <kangdk02@naver.com>
2022-08-15 18:11:43 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 452 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    int T, A, B, i;
+    scanf("%d", &T);
+    for (i = 0; i < T; i++) {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A + B);
+    }
+}
