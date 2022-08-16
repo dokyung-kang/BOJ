5cff68d883d69fecf0942c11364bc4b476d78240
dokyung-kang <kangdk02@naver.com>
2022-08-16 19:55:37 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int N;
+	int i;
+
+	scanf("%d", &N);
+
+	for (i = 2; i <= N; i++) {
+		while (N % i == 0) {
+			printf("%d\n", i);
+			N /= i;
+		}
+	}
+}
