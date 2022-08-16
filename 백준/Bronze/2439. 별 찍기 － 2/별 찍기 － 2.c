80d61f0063f3a948239d791f8dad55cd9ed4688c
dokyung-kang <kangdk02@naver.com>
2022-08-17 00:01:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i, j, N;
+	scanf("%d", &N);
+
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N - i - 1; j++)
+			printf(" ");
+		for (j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+}
