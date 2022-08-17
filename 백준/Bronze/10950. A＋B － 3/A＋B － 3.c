8880054fbec6c48b0bd08f68c4fa9431b9f7de00
dokyung-kang <kangdk02@naver.com>
2022-08-18 00:48:20 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i, T, A, B;
+
+	scanf("%d", &T);
+	for (i = 0; i < T; i++)
+	{
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}
+}
