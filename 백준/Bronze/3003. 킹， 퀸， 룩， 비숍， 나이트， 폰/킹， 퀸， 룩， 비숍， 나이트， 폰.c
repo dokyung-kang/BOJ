264bcb2fa764e16df708f0aa5d1425af6b7223ab
dokyung-kang <kangdk02@naver.com>
2022-08-20 00:04:34 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) 
+{
+	int k, q, l, b, n, p;
+
+	scanf("%d %d %d %d %d %d", &k, &q, &l, &b, &n, &p);
+
+	printf("%d %d %d %d %d %d", 1 - k, 1 - q, 2 - l, 2 - b, 2 - n, 8 - p);
+}
