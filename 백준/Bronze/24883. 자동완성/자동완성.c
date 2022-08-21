36fdf5b477fd6dd3c63595373ed6e18e8306089f
dokyung-kang <kangdk02@naver.com>
2022-08-22 00:39:03 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	char a;
+
+	scanf("%c", &a);
+
+	if (a == 'N' || a == 'n')
+		printf("Naver D2");
+	else
+		printf("Naver Whale");
+}
