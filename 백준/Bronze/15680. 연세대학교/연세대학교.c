af82c8433049ddcdd83c4db0137ffc05d9adc44d
dokyung-kang <kangdk02@naver.com>
2022-08-22 00:27:50 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n == 0)
+		printf("YONSEI");
+	if (n == 1)
+		printf("Leading the Way to the Future");
+}
