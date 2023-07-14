c05caee29f47839f773e76b3e37391cb7b43b6b2
강도경 <kangdk02@naver.com>
2023-07-15 00:27:26 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[1001];
+    int n;
+    
+    scanf("%s", &str);
+    scanf("%d", &n);
+    
+    printf("%c", str[n - 1]);
+}
