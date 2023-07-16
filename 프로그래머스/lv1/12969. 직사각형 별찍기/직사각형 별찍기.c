728f0b96ee8bf84e098c616547ce38e7547905b1
강도경 <kangdk02@naver.com>
2023-07-16 16:16:33 +0900

    [level 1] Title: 직사각형 별찍기, Time: 1.70 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    int i, j;
+    
+    scanf("%d %d", &a, &b);
+    
+    
+    for (i = 0; i < b; i++){
+        for (j = 0; j < a; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
