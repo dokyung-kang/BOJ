225752c2894146e37e1e208b53f5ec5dbdc1070d
강도경 <kangdk02@naver.com>
2023-10-03 16:54:04 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int n;
+	int tempN;	// N의 진짜 약수
+	int max = 0;
+	int min = 1000000;
+	int result;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%d", &tempN);
+		if (tempN >= max)
+			max = tempN;
+		if (tempN <= min)
+			min = tempN;
+	}
+
+	result = max * min;
+	printf("%d", result);
+}
