82cf722f2ec5867367d91355b37ec73f1874a4d2
강도경 <kangdk02@naver.com>
2023-11-14 00:50:36 +0900

    [level unrated] Title: 모음 제거, Time: 9.11 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        int i;
+        
+        for(i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) == 'a' || my_string.charAt(i) == 'e' || 
+              my_string.charAt(i) == 'i' || my_string.charAt(i) == 'o' || 
+              my_string.charAt(i) == 'u')
+                continue;
+            answer += my_string.charAt(i);
+        
+        }
+        return answer;
+    }
+}
