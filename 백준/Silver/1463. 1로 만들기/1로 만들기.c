31a053fd5b138927d56cc745d5076c3ad7e059a2
강도경 <kangdk02@naver.com>
2023-11-25 17:24:34 +0900

    [Silver III] Title: 1로 만들기, Time: 28 ms, Memory: 32244 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.c
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.c
@@ -1,40 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-long long cal(long long n, long long* M)
+long long cal(long long n, long long* m)
 {
-
 	long long min = n;
+
 	if (n == 1)
 		return 0;
+	
 	if (n % 3 == 0) {
-		if (M[n / 3] == -1)
-			M[n / 3] = cal(n / 3, M);
-		if (M[n / 3] < min)
-			min = M[n / 3];
+		if (m[n / 3] == -1)
+			m[n / 3] = cal(n / 3, m);
+		if (m[n / 3] < min)
+			min = m[n / 3];
 	}
+
 	if (n % 2 == 0) {
-		if (M[n / 2] == -1)
-			M[n / 2] = cal(n / 2, M);
-		if (M[n / 2] < min)
-			min = M[n / 2];
+		if (m[n / 2] == -1)
+			m[n / 2] = cal(n / 2, m);
+		if (m[n / 2] < min)
+			min = m[n / 2];
 	}
-	if (M[n - 1] == -1)
-		M[n - 1] = cal(n - 1, M);
-	if (M[n - 1] < min)
-		min = M[n - 1];
+
+	if (m[n - 1] == -1)
+		m[n - 1] = cal(n - 1, m);
+	if (m[n - 1] < min)
+		min = m[n - 1];
+
 	return min + 1;
 }
 
 int main(void)
 {
 	long long n;
-	long long* M;
+	long long* m;
 	int i;
+	long long result;
 
 	scanf("%lld", &n);
-	M = (long long*)malloc(sizeof(long long) * n);
+
+	m = (long long*)malloc(sizeof(long long) * n);
+
 	for (i = 0; i < n; i++)
-		M[i] = -1;
-	printf("%lld", cal(n, M));
+		m[i] = -1;
+
+	result = cal(n, m);
+
+	printf("%lld", result);
 }
